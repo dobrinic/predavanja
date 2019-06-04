d83ca5bd2bd2273f5208bbad972327368f39da19
Aleksandar Dobrinić <dobrinich@gmail.com>
2019-06-04 11:35:12 +0200

    Updated script tvrtka.sql with timestamps creation

--- mysql/tvrtka.sql
+++ mysql/tvrtka.sql
@@ -1,31 +1,36 @@
-
 CREATE DATABASE IF NOT EXISTS tvrtka DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-
 USE tvrtka;
 
 CREATE TABLE IF NOT EXISTS grad (
     id INT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    ime VARCHAR(50) NOT NULL ) ENGINE=InnoDB;
+    ime VARCHAR(50) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP on update CURRENT_TIMESTAMP NULL DEFAULT NULL) ENGINE=InnoDB;
 
 CREATE TABLE IF NOT EXISTS poslovnica (
     id INT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
     naziv VARCHAR(10) NOT NULL,
     grad_id INT UNSIGNED NOT NULL,
-    FOREIGN KEY (grad_id) REFERENCES grad(id) ) ENGINE=InnoDB;
+    FOREIGN KEY (grad_id) REFERENCES grad(id),
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP on update CURRENT_TIMESTAMP NULL DEFAULT NULL) ENGINE=InnoDB;
 
 CREATE TABLE IF NOT EXISTS zaposlenici (
     id INT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
     ime VARCHAR(20) NOT NULL,
     prezime VARCHAR(20) NOT NULL,
     poslovnica_id INT UNSIGNED NOT NULL,
-    FOREIGN KEY (poslovnica_id) REFERENCES poslovnica(id) ) ENGINE=InnoDB;
+    FOREIGN KEY (poslovnica_id) REFERENCES poslovnica(id),
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP on update CURRENT_TIMESTAMP NULL DEFAULT NULL) ENGINE=InnoDB;
 
 CREATE TABLE IF NOT EXISTS voditelj_poslovnice (
     zaposlenik_id INT UNSIGNED NOT NULL,
     FOREIGN KEY (zaposlenik_id) REFERENCES zaposlenici(id),
     poslovnica_id INT UNSIGNED NOT NULL,    
-    FOREIGN KEY (poslovnica_id) REFERENCES poslovnica(id)
-    ) ENGINE=InnoDB;
+    FOREIGN KEY (poslovnica_id) REFERENCES poslovnica(id),
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP on update CURRENT_TIMESTAMP NULL DEFAULT NULL) ENGINE=InnoDB;
 
 
 INSERT INTO grad (ime) VALUES ('Zagreb'), ('Pula'), ('Split'), ('Varaždin'), ('Sisak');
@@ -41,6 +46,6 @@ INSERT INTO zaposlenici
 INSERT INTO voditelj_poslovnice (zaposlenik_id, poslovnica_id)
     VALUES (2,2), (4,1);
 
-ALTER TABLE zaposlenici ADD godine INT;
+ALTER TABLE zaposlenici ADD godine INT AFTER prezime;
 
 UPDATE zaposlenici SET godine = RAND()*(30)+20;
