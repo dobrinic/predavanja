b143294399b571769f87f6d3ad328d5aa0cc85c8
programer10 <programer10@ucione.local>
2019-06-17 18:59:35 +0200

    finished singelton class

--- php_mysql/dan2/singleton.php
+++ php_mysql/dan2/singleton.php
@@ -42,7 +42,14 @@ class DB {
     public function delete($table, $where){
         // DELETE FROM $table $where;
 
-        $query =
+        if (count($where) === 3) {
+            
+            $field = $where[0];
+            $operator = $where[1];
+            $value = $where[2];
+
+            $query = "DELETE FROM $table WHERE $field $operator $value";
+        }
 
         $result = $this->connection->prepare($query);
 
@@ -51,12 +58,30 @@ class DB {
         return $result->fetchAll(PDO::FETCH_OBJ);
     }
 
-    public function insert($table, $cloumns){
+    public function insert($table, $columns){
         // INSERT INTO $table ($columns->keys) VALUES ($columns->vrijednosti)
         // insert('zaposlenici', [ 'ime' => 'Vijeko', 'prezime' => 'Vjekić' ]);
         // INSERT INTO zaposlenici ('ime', 'prezime') VALUES ('Vijeko', 'Vjekić');
 
-        $query =
+        $keys = implode(",", array_keys($columns));
+        $field_num = count($columns);
+        $values = '';
+        $counter = 1;
+
+        foreach ($columns as $key => $value) {
+
+            if (is_string($value)) {
+                $value = "'$value'";
+            }
+
+            $values .= $value;
+            if ($counter < $field_num) {
+                $values .= ',';
+            }
+            $counter++;
+        }
+
+        $query = "INSERT INTO $table ($keys) VALUES ($values)";
 
         $result = $this->connection->prepare($query);
 
@@ -69,7 +94,16 @@ class DB {
     public function get($columns, $table, $where = array()){
         // get('ime, prezime', 'zaposlenici', ['id', '=', '2']);
 
-        $query =
+        if (count($where) === 3) {
+            
+            $field = $where[0];
+            $operator = $where[1];
+            $value = $where[2];
+
+            $query = "SELECT $columns FROM $table WHERE $field $operator $value";
+        } else {
+            $query = "SELECT $columns FROM $table";
+        }
 
         $result = $this->connection->prepare($query);
 
@@ -80,39 +114,88 @@ class DB {
 
     public function update($table, $fields, $id){
         // UPDATE $table SET $fields WHERE id=$id;
+
+        $field_num = count($fields);
+        $values = '';
+        $counter = 1;
+
+        foreach ($fields as $key => $value) {
+
+            if (is_string($value)) {
+                $value = "'$value'";
+            }
+
+            $values .= "$key = $value";
+            if ($counter < $field_num) {
+                $values .= ',';
+            }
+            $counter++;
+        }
+
+        $query = "UPDATE $table SET $values WHERE id=$id";
+
+        $result = $this->connection->prepare($query);
+
+        $result->execute();
+
+        return $result->fetchAll(PDO::FETCH_OBJ);
     }
-
-
-
 }
 
-DB::getInstance()->insert('zaposlenici', [ 'ime' => 'Vijeko', 'prezime' => 'Vjekić' ,.....]);
+//DB::getInstance()->insert('zaposlenici', [ 'ime' => 'Vijeko', 'prezime' => 'Vjekić' , 'godine' => 40, 'poslovnica_id' => 2]);
 
-
-
+//DB::getInstance()->update('zaposlenici', [ 'ime' => 'Marko', 'prezime' => 'Markić'], 7);
 
 class Zaposlenik {
 
     private $db;
+    private $ime;
+    private $prezime = 'Dobrinic';
+    private $godine = 34;
 
-    function __construct($db){
-        $this->db = $db;
+    function __construct(){
+        $this->db = DB::getInstance();
     }
 
     public function pronadji_zaposlenika($id){
-        $zaposlenik = $this->db->query("*" , "zaposlenici", "id=$id");
+        $zaposlenik = $this->db->get("*" , "zaposlenici", ['id', '=', $id]);
         return $zaposlenik;
     }
+
+    public function pronadji_zaposlenike(){
+        $zaposlenik = $this->db->query("*" , "zaposlenici");
+        return $zaposlenik;
+    }
+
+    public function obrisi_zaposlenika($id){
+        $zaposlenik = $this->db->delete('zaposlenici', ['id', '=', $id]);
+        return $zaposlenik;
+    }
+
+    public function azuriraj_zaposlenika($id){
+        $zaposlenik = $this->db->update('zaposlenici', $fields, $id);
+        return $zaposlenik;
+    }
+
+    public function umetni_zaposlenika($columns){
+        $zaposlenik = $this->db->insert('zaposlenici', $columns);
+        return $zaposlenik;
+    }
+
 }
 
-$database = DB::getInstance();
+$zaposlenici = new Zaposlenik();
 
-$zaposlenik = new Zaposlenik($database);
+$zaposlenik = $zaposlenici->pronadji_zaposlenike();
 
-$rez = $zaposlenik->pronadji_zaposlenika(3);
+//$zaposlenik = $zaposlenici->azuriraj_zaposlenika(6, [ 'ime' => 'Marko', 'prezime' => 'Markić']);
 
-foreach ($rez as $key => $value) {
+//$zaposlenik = $zaposlenici->umetni_zaposlenika([ 'ime' => 'Vijeko', 'prezime' => 'Vjekić' , 'godine' => 40, 'poslovnica_id' => 2]);
+
+//$zaposlenik = $zaposlenici->obrisi_zaposlenika(3);
+
+foreach ($zaposlenik as $key => $value) {
     echo '<p>';
-    echo ++$key . '. ' . $value->ime . ' ' . $value->prezime;
+    echo $value->id . '. ' . $value->ime . ' ' . $value->prezime;
     echo '</p>';
 }
